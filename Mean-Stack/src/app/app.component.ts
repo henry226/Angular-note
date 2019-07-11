34ee28ebd430b8002e890f3f6ec27060a1907ab8
Henry Li <henry5581984@gmail.com>
2019-07-11 15:26:05 -0400

    event binding

--- Mean-Stack/src/app/app.component.ts
+++ Mean-Stack/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mean-Stack';
+  title = [];
+
+  onPostAdded(post){
+    this.title.push(post);
+  }
 }
