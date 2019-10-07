f8a6ca627372772ff0e69e397066d967e40f317b
Henry Li <henry5581984@gmail.com>
2019-10-06 20:21:34 -0400

    data table

---
+++ Angular-Material/Data_Table/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule,
+         MatPaginatorModule,
+         MatSortModule,
+         MatFormFieldModule,
+         MatInputModule,
+         MatSelectModule,
+         MatCheckboxModule
+} from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
