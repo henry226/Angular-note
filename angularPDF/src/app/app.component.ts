272e33422bad41262191ab0c050fd1074a61e841
Henry Li <henry5581984@gmail.com>
2020-05-15 15:25:58 -0400

    adding basic functionality

--- angularPDF/src/app/app.component.ts
+++ angularPDF/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularPDF';
+  public hostUrl = 'https://ej2-aspcore-service.azurewebsites.net/';
+  public ajaxSettings: object = {
+    url: this.hostUrl +  'api/FileManager/FileOperations',
+    uploadUrl: this.hostUrl + 'api/FileManager/Upload',
+    getImageUrl: this.hostUrl + 'api/FileManager/GetImage',
+    downloadUrl: this.hostUrl + 'api/FileManager/Download'
+  };
 }
